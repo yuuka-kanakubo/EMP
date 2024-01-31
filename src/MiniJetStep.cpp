4de7498245e471a4e660193b206a648fb7ee6a0a
yuuka-kanakubo <yuka.y.kanakubo@jyu.fi>
2024-01-31 10:36:03 +0200

    MiniJetStep.cppÖ: debugged.

--- src/MiniJetStep.cpp
+++ src/MiniJetStep.cpp
@@ -17,10 +17,10 @@ std::cout << ":) Minijet::Step is called." << std::endl;
 		double vx = part.px/part.e;
 		double vy = part.py/part.e;
 		double vz = part.pz/part.e;
-		part.x = constants::tau0*cosh(part.rap)*vx;
-		part.y = constants::tau0*cosh(part.rap)*vy;
-		part.z = constants::tau0*cosh(part.rap)*vz;
-		part.t = constants::tau0*cosh(part.rap);
+		part.x = part.x + constants::tau0*cosh(part.rap)*vx;
+		part.y = part.y + constants::tau0*cosh(part.rap)*vy;
+		part.z = 0.0 + constants::tau0*cosh(part.rap)*vz;
+		part.t = 0.0 + constants::tau0*cosh(part.rap);
 
 
 

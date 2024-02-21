b7ae4af6dcdc4a34ffde8c4fb24abab475010c30
yuuka-kanakubo <yuka.y.kanakubo@jyu.fi>
2024-02-21 17:18:01 +0200

    Compiled.

--- include/commentread.h
+++ include/commentread.h
@@ -1,102 +1,11 @@
 // written by h. niemi
-#ifndef COMMREAD_H
-#define COMMREAD_H
 #include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
 
+class commentread{
 double commentfloatread(FILE *stream);
 int commentintread(FILE *stream);
 int commentstrread(FILE *stream, char *buf);
 
-
-double commentfloatread(FILE *stream)
-{
-	char cache1[500];
-	int check;
-	double value;
-	int out_flag;
-	int err;
-
-	value = 0.0;
-	out_flag = 0;
-	while(!out_flag)
-	{
-		err = fscanf(stream,"%s", cache1);
-
-		if(cache1[0]=='#')
-			while((check = fgetc(stream)) != '\n' && check != EOF);
-		else
-		{
-			value = atof(cache1);
-			out_flag = 1;
-		}
-	}
-
-	return value;
-}
-
-
-int commentintread(FILE *stream)
-{
-	char cache1[500];
-	int value, check;
-	int out_flag;
-	int err;
-
-	value = 0;
-	out_flag = 0;
-	while(!out_flag)
-	{
-		err = fscanf(stream,"%s", cache1);
-
-		if(cache1[0]=='#')
-			while((check = fgetc(stream)) != '\n' && check != EOF);
-		else
-		{
-			value = atoi(cache1);
-			out_flag = 1;
-		}
-	}
-
-	return value;
-}
-
-
-int commentstrread(FILE *stream, char *buf)
-{
-	char cache1[500];
-	int check;
-	int out_flag;
-	int err;
-
-	out_flag = 0;
-	while(!out_flag)
-	{
-		err = fscanf(stream,"%s", cache1);
-
-		if(cache1[0]=='#')
-			while((check = fgetc(stream)) != '\n' && check != EOF);
-		else
-		{
-			strcpy(buf, cache1);
-			out_flag = 1;
-		}
-	}
-
-	return 0;
-}
-#endif
-
-// Recognize empty line
-// {
-// 	char buf[BUFLEN], *p;
-// 	int counter = 0;
-// 	while( !feof(stdin) ){
-// 		fgets(buf, BUFLEN, stdin);
-// 		p=buf;
-// 		while(*p==' ' || *p=='\t') p++; // skip whitespaces
-// 		if(*p=='\r') p++;
-// 		if(*p=='\n') counter++;
-// 	}
-// 	printf("%d\n", counter);
-// 	return 0;
-// }
+};
---
+++ src/eostableclasses.cpp
@@ -0,0 +1,8 @@
+#include "eostableclasses.h"
+
+
+EOS::EOS(int NX, int NY){
+
+
+
+}
